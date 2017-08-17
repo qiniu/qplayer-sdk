b860093073fbd363b5faa9c8ee242e9b7458b632
jinbangfei <jinbangfei@qiniu.com>
2017-08-17 19:58:54 +0800

    Add SEI DATA output.

--- android/qplayerTest/app/src/main/java/com/qiniu/qplayer/mediaEngine/BasePlayer.java
+++ android/qplayerTest/app/src/main/java/com/qiniu/qplayer/mediaEngine/BasePlayer.java
@@ -176,6 +176,7 @@ public interface BasePlayer {
 
 
 	public static final int 	QC_FLAG_Video_CaptureImage		= 0x00000010;
+	public static final int 	QC_FLAG_Video_SEIDATA			= 0x00000020;
 	public static final int 	QC_FLAG_Video_YUV420P			= 0x00000000;
 
 	// The event listener function
